fb71203b9684ad02aa72ec3f20bfb2502ebb9a95
shackijj <shaackiy@gmail.com>
2016-04-01 19:20:24 +0300

    p20 initial

---
+++ p20_continuous_integration/src/test/ValidatorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("userthing/persist/UserStore.php");
+require_once("userthing/util/Validator.php");
+
+class ValidatorTest extends PHPUnit_Framework_TestCase {
+    private $validator;
+
+    public function setUp() {
+        $store = new UserStore();
+        $store->addUser( "bob williams", "bob@example.com", "12345" );
+        $this->validator = new Validator( $store );
+    }
+
+    public function tearDown() {
+    }
+
+    public function testValidate_CorrectPass() {
+        $this->assertTrue( 
+                $this->validator->validateUser( "bob@example.com", "12345" ),
+                    "Successful check expected.");
+    }
+    
+    public function testValidate_FailedPass() {
+        $this->assertFalse(
+                $this->validator->validateUser( "bob@example.co", "12" ),
+                "Failed check expected.");
+    }
+                
+
+    public function testValidate_FalsePass() {
+        $store = $this->getMock("UserStore");
+        $this->validator = new Validator( $store );
+        $store->expects( $this->once() )
+            ->method('notifyPasswordFailure')
+            ->with( $this->equalTo('bob@example.com') );
+
+        $store->expects( $this->any() )
+            ->method("getUser")
+            ->will( $this->returnValue( 
+                        new User( "bob", "bob@example", "rigth")));
+
+        $this->validator->validateUser("bob@example.com", "wrong");
+    }
+}
+?>
